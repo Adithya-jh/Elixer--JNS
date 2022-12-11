bd1fdd46aaec37bdd364008d50ce4a24ff995b78
Adithya <jhadith88@gmail.com>
2022-12-11 14:06:31 +0530

    updated

--- JNS-BC/contracts/Domains.sol
+++ JNS-BC/contracts/Domains.sol
@@ -17,6 +17,26 @@ contract Domains is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  modifier onlyOwner() {
+  require(isOwner());
+  _;
+}
+
+error Unauthorized();
+error AlreadyRegistered();
+error InvalidName(string name);
+
+function isOwner() public view returns (bool) {
+  return msg.sender == owner;
+}
+
+function withdraw() public onlyOwner {
+  uint amount = address(this).balance;
+  
+  (bool success, ) = msg.sender.call{value: amount}("");
+  require(success, "Failed to withdraw Matic");
+} 
+
   // We'll be storing our NFT images on chain as SVGs
   string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#fff"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#cb5eee"/><stop offset="1" stop-color="#0cd7e4" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
   string svgPartTwo = '</text></svg>';
@@ -24,14 +44,30 @@ contract Domains is ERC721URIStorage {
   
   // Checkout our new mapping! This will store values
   mapping(string => string) public records;
+  // Add this at the top of your contract next to the other mappings
+  mapping (uint => string) public names;
     // Here's our domain TLD!
   string public tld;
 
-    // We make the contract "payable" by adding this to the constructor
-  constructor(string memory _tld) payable ERC721("JHA name service", "JNS") {
+ address payable public owner;
+
+  constructor(string memory _tld) ERC721 ("JHA Name Service", "JNS") payable {
+    owner = payable(msg.sender);
     tld = _tld;
     console.log("%s name service deployed", _tld);
   }
+
+  // Add this anywhere in your contract body
+function getAllNames() public view returns (string[] memory) {
+  console.log("Getting all names from contract");
+  string[] memory allNames = new string[](_tokenIds.current());
+  for (uint i = 0; i < _tokenIds.current(); i++) {
+    allNames[i] = names[i];
+    console.log("Name for token %d is %s", i, allNames[i]);
+  }
+
+  return allNames;
+}
 
 
    // This function will give us the price of a domain based on length
@@ -49,6 +85,8 @@ contract Domains is ERC721URIStorage {
 
  // Added "payable" modifier to register function
   function register(string calldata name) public payable{
+    if (domains[name] != address(0)) revert AlreadyRegistered();
+    if (!valid(name)) revert InvalidName(name);
        require(domains[name] == address(0));
 
     uint256 _price = price(name);
@@ -87,9 +125,14 @@ contract Domains is ERC721URIStorage {
     _safeMint(msg.sender, newRecordId);
     _setTokenURI(newRecordId, finalTokenUri);
     domains[name] = msg.sender;
+    names[newRecordId] = name;
 
     _tokenIds.increment();
   }
+
+  function valid(string calldata name) public pure returns(bool) {
+  return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 10;
+}
 
 
 
@@ -97,11 +140,10 @@ contract Domains is ERC721URIStorage {
       return domains[name];
   }
 
-  function setRecord(string calldata name, string calldata record) public {
-      // Check that the owner is the transaction sender
-      require(domains[name] == msg.sender);
-      records[name] = record;
-  }
+function setRecord(string calldata name, string calldata record) public {
+  if (msg.sender != domains[name]) revert Unauthorized();
+  records[name] = record;
+}
 
   function getRecord(string calldata name) public view returns(string memory) {
       return records[name];
--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./styles/App.css";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import contractAbi from "./utils/contractAbi.json";
+
+import { AnimatePresence, motion } from "framer-motion/dist/framer-motion";
+
+import { networks } from "./utils/networks";
 // Add the domain you will be minting
 const tld = ".jha";
 const CONTRACT_ADDRESS = "0x9233A4b7B05e2c74d343cEc81D8c2c336dBc4FD3";
@@ -11,10 +15,20 @@ const App = () => {
   //Just a state variable we use to store our user's public wallet. Don't forget to import useState at the top.
   const [currentAccount, setCurrentAccount] = useState("");
 
+  // Add a new stateful variable at the start of our component next to all the old ones
+  const [editing, setEditing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // Add a stateful array at the top next to all the other useState calls
+  const [mints, setMints] = useState([]);
+
   // Add some state data propertie
   const [domain, setDomain] = useState("");
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [record, setRecord] = useState("");
+
+  // Create a stateful variable to store the network next to all the others
+  const [network, setNetwork] = useState("");
 
   // Implement your connectWallet method here
   const connectWallet = async () => {
@@ -34,6 +48,44 @@ const App = () => {
       // Boom! This should print out public address once we authorize Metamask.
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchMints = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // You know all this
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        // Get all the domain names from our contract
+        const names = await contract.getAllNames();
+
+        // For each name, get the record and the address
+        const mintRecords = await Promise.all(
+          names.map(async (name) => {
+            const mintRecord = await contract.records(name);
+            const owner = await contract.domains(name);
+            return {
+              id: names.indexOf(name),
+              name: name,
+              record: mintRecord,
+              owner: owner,
+            };
+          })
+        );
+
+        console.log("MINTS FETCHED ", mintRecords);
+        setMints(mintRecords);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -60,6 +112,17 @@ const App = () => {
     } else {
       console.log("No authorized account found");
     }
+
+    // This is the new part, we check the user's network chain ID
+    const chainId = await ethereum.request({ method: "eth_chainId" });
+    setNetwork(networks[chainId]);
+
+    ethereum.on("chainChanged", handleChainChanged);
+
+    // Reload the page when they change networks
+    function handleChainChanged(_chainId) {
+      window.location.reload();
+    }
   };
   // Create a function to render if wallet is not connected yet
   const renderNotConnectedContainer = () => (
@@ -76,6 +139,13 @@ const App = () => {
       </button>
     </div>
   );
+
+  // This will run any time currentAccount or network are changed
+  useEffect(() => {
+    if (network === "Polygon Mumbai Testnet") {
+      fetchMints();
+    }
+  }, [currentAccount, network]);
 
   const mintDomain = async () => {
     // Don't run if the domain is empty
@@ -124,6 +194,11 @@ const App = () => {
             "Record set! https://mumbai.polygonscan.com/tx/" + tx.hash
           );
 
+          // Call fetchMints after 2 seconds
+          setTimeout(() => {
+            fetchMints();
+          }, 2000);
+
           setRecord("");
           setDomain("");
         } else {
@@ -135,11 +210,61 @@ const App = () => {
     }
   };
 
+  const updateDomain = async () => {
+    if (!record || !domain) {
+      return;
+    }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        fetchMints();
+        setRecord("");
+        setDomain("");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
   // Form to enter domain name and data
   const renderInputForm = () => {
+    // If not on Polygon Mumbai Testnet, render the switch button
+    if (network !== "Polygon Mumbai Testnet") {
+      return (
+        <div className="connect-wallet-container">
+          <h2>Please switch to Polygon Mumbai Testnet</h2>
+          {/* This button will call our switch network function */}
+          <button className="cta-button mint-button" onClick={switchNetwork}>
+            Click here to switch
+          </button>
+        </div>
+      );
+    }
+
     return (
       <div className="form-container">
-        <div className="first-row">
+        <motion.div
+          className="first-row"
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 0.25 },
+          }}
+        >
           <input
             type="text"
             value={domain}
@@ -147,34 +272,117 @@ const App = () => {
             onChange={(e) => setDomain(e.target.value)}
           />
           <p className="tld"> {tld} </p>
-        </div>
+        </motion.div>
 
-        <input
-          type="text"
-          value={record}
-          placeholder="whats ur fav movie"
-          onChange={(e) => setRecord(e.target.value)}
-        />
-
-        <div className="button-container">
-          <button
-            className="cta-button mint-button"
-            disabled={null}
-            // onClick={null}
-            onClick={mintDomain}
+        <motion.div
+          whileHover={{
+            scale: 1.5,
+            transition: { duration: 0.25 },
+          }}
+        >
+          <input
+            type="text"
+            value={record}
+            placeholder="WHAT IS UR FAV MOVIE?"
+            onChange={(e) => setRecord(e.target.value)}
+          />
+        </motion.div>
+        {/* If the editing variable is true, return the "Set record" and "Cancel" button */}
+        {editing ? (
+          <div className="button-container">
+            {/* This will call the updateDomain function we just made */}
+            <button
+              className="cta-button mint-button"
+              disabled={loading}
+              onClick={updateDomain}
+            >
+              Set record
+            </button>
+            {/* This will let us get out of editing mode by setting editing to false */}
+            <button
+              className="cta-button mint-button"
+              onClick={() => {
+                setEditing(false);
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          // If editing is not true, the mint button will be returned instead
+          <motion.div
+            whileHover={{
+              scale: 1.5,
+              transition: { duration: 0.25 },
+            }}
+            whileTap={{ scale: 0.8 }}
           >
-            Mint
-          </button>
-          <button
-            className="cta-button mint-button"
-            disabled={null}
-            onClick={null}
-          >
-            Set data
-          </button>
-        </div>
+            <button
+              className="cta-button mint-button"
+              disabled={loading}
+              onClick={mintDomain}
+            >
+              Mint
+            </button>
+          </motion.div>
+        )}
       </div>
     );
+  };
+
+  // Add this render function next to your other render functions
+  const renderMints = () => {
+    if (currentAccount && mints.length > 0) {
+      return (
+        <div className="mint-container">
+          <p className="subtitle"> Recently minted domains!</p>
+          <div className="mint-list">
+            {mints.map((mint, index) => {
+              return (
+                <div className="mint-item" key={index}>
+                  <div className="mint-row">
+                    <a
+                      className="link"
+                      href={`https://testnets.opensea.io/assets/mumbai/${CONTRACT_ADDRESS}/${mint.id}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <p className="underlined">
+                        {" "}
+                        {mint.name}
+                        {tld}{" "}
+                      </p>
+                    </a>
+                    {/* If mint.owner is currentAccount, add an "edit" button*/}
+                    {mint.owner.toLowerCase() ===
+                    currentAccount.toLowerCase() ? (
+                      <button
+                        className="edit-button"
+                        onClick={() => editRecord(mint.name)}
+                      >
+                        <img
+                          className="edit-icon"
+                          src="https://img.icons8.com/metro/26/000000/pencil.png"
+                          alt="Edit button"
+                        />
+                      </button>
+                    ) : null}
+                  </div>
+                  <p> {mint.record} </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    }
+  };
+
+  // This will take us into edit mode and show us the edit buttons!
+  const editRecord = (name) => {
+    console.log("Editing record for", name);
+    setEditing(true);
+    setDomain(name);
   };
 
   // This runs our function when the page loads.
@@ -182,21 +390,70 @@ const App = () => {
     checkIfWalletIsConnected();
   }, []);
 
+  const switchNetwork = async () => {
+    if (window.ethereum) {
+      try {
+        // Try to switch to the Mumbai testnet
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: "0x13881" }], // Check networks.js for hexadecimal network ids
+        });
+      } catch (error) {
+        // This error code means that the chain we want has not been added to MetaMask
+        // In this case we ask the user to add it to their MetaMask
+        if (error.code === 4902) {
+          try {
+            await window.ethereum.request({
+              method: "wallet_addEthereumChain",
+              params: [
+                {
+                  chainId: "0x13881",
+                  chainName: "Polygon Mumbai Testnet",
+                  rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+                  nativeCurrency: {
+                    name: "Mumbai Matic",
+                    symbol: "MATIC",
+                    decimals: 18,
+                  },
+                  blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+                },
+              ],
+            });
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        console.log(error);
+      }
+    } else {
+      // If window.ethereum is not found then MetaMask is not installed
+      alert(
+        "MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html"
+      );
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <header>
-            <div className="left">
-              <p className="title">🐱‍👤 JHA NAME SERVICE</p>
+            <motion.div
+              animate={{ x: 300 }}
+              transition={{ type: "spring", stiffness: 100 }}
+              className="left"
+            >
+              <p className="title">
+                <span>😎</span> JHA NAME SERVICE
+              </p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
-            </div>
+            </motion.div>
           </header>
         </div>
 
         {!currentAccount && renderNotConnectedContainer()}
-        {/* Render the input form if an account is connected */}
         {currentAccount && renderInputForm()}
+        {mints && renderMints()}
       </div>
     </div>
   );
